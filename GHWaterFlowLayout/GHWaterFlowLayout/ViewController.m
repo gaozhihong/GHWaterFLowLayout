c7de5680f3cc1443c5963727061e410fa638a997
gaozhihong <18710296168@163.com>
2018-01-27 08:51:35 +0800

    com-1

--- GHWaterFlowLayout/GHWaterFlowLayout/ViewController.m
+++ GHWaterFlowLayout/GHWaterFlowLayout/ViewController.m
@@ -23,7 +23,6 @@ NSString *cellId = @"cellId";
     // Do any additional setup after loading the view, typically
     self.view.backgroundColor = [UIColor whiteColor];
     [self setupCollectionView];
-     // chedan  
     
 }
 -(void)setupCollectionView{
